02ff84a490d995cac1cb435b42c88b06d67a556c
kavita <sehgalkavita27@gmail.com>
2023-01-20 21:10:26 +0530

    by nam

--- Chaitanya_229140/Productmanagment/Model/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace Productmanagment.Model;
-
-public class Product{
-     
-     public string title {get;set;}
-     public string description {get;set;}
-     public double  unitprice {get;set;}
-     public double balance {get;set;}
-}
